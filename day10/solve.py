cb54ce2e8ee22236b6d50b6c47d22b4b9fec2993
Max Larsson <maxslarsson@gmail.com>
2021-12-10 00:40:03 -0500

    Solved part2 of day10

--- day10/solve.py
+++ day10/solve.py
@@ -29,11 +29,44 @@ def part1(s):
     return sum(lookup_table[c] for c in first_invalid_chars)
 
 
+def part2(s):
+    missing_closing_characters = []
+    for line in s:
+        stack = []
+        for c in line:
+            if c in opposite.keys():
+                stack.append(c)
+            elif c == opposite[stack[-1]]:
+                stack.pop()
+            else:
+                break
+        else:
+            missing_closing_characters.append(reversed([opposite[c] for c in stack]))
+
+    lookup_table = {
+        ')': 1,
+        ']': 2,
+        '}': 3,
+        '>': 4
+    }
+
+    total_scores = []
+    for line in missing_closing_characters:
+        total_score = 0
+        for c in line:
+            total_score *= 5
+            total_score += lookup_table[c]
+        total_scores.append(total_score)
+    total_scores = sorted(total_scores)
+    return total_scores[len(total_scores)//2]
+
+
 def main():
     with open('input.txt') as f:
         s = f.read().split("\n")
 
     print(f"Part 1: {part1(s)}")
+    print(f"Part 2: {part2(s)}")
 
 
 if __name__ == '__main__':
