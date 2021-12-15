3e565db1db4ac1d6c32b581aff69e8d36a932e8e
Max Larsson <maxslarsson@gmail.com>
2021-12-15 11:49:57 -0500

    Solved part2 of day12

--- day12/solve.py
+++ day12/solve.py
@@ -4,6 +4,7 @@
 INFILE = 'input.txt'
 
 NUM_PATHS_PART_1 = 0
+NUM_PATHS_PART_2 = 0
 
 
 def part1(graph, starting_node="start", visited=None):
@@ -29,6 +30,34 @@ def part1(graph, starting_node="start", visited=None):
     return NUM_PATHS_PART_1
 
 
+def part2(graph, starting_node="start", visited=None):
+    global NUM_PATHS_PART_2
+    if visited is None:
+        visited = []
+
+    visited.append(starting_node)
+
+    to_visit = graph[starting_node]
+    for v in to_visit:
+        if v == "end":
+            NUM_PATHS_PART_2 += 1
+        elif v.isupper():
+            part2(graph, v, visited)
+        elif v.islower():
+            times_visited = visited.count(v)
+            if times_visited == 0:
+                part2(graph, v, visited)
+            elif times_visited == 1 and v != "start":
+                small_caves = list(filter(lambda x: x.islower(), visited))
+                counts = map(lambda x: small_caves.count(x), small_caves)
+                if 2 not in counts:
+                    part2(graph, v, visited)
+
+    visited.pop()
+
+    return NUM_PATHS_PART_2
+
+
 def main():
     with open(INFILE) as f:
         graph = {}
@@ -44,6 +73,7 @@ def main():
                 graph[b] = [a]
 
     print(f"Part 1: {part1(graph)}")
+    print(f"Part 2: {part2(graph)}")
 
 
 if __name__ == '__main__':
